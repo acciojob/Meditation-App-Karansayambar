e006eb3a1215f81bf1d4f8075718a9f82935c875
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-15 15:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+	const sound1 = document.getElementById("sound1");
+	const sound2 = document.getElementById("sound2");
+	const app = document.getElementById("app");
+	sound1.addEventListener("click", function(){
+		const music1 = document.getElementById("meditationAudio");
+		const video1 = document.getElementById("meditationVideo");
+		app.appendChild(video1);
+	})
+})
